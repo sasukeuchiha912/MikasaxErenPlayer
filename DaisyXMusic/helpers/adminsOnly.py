7aef4ecce789251b88e4e2c8217f377aca658afe
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-07-14 02:15:18 +0530

    Update adminsOnly.py

--- DaisyXMusic/helpers/adminsOnly.py
+++ DaisyXMusic/helpers/adminsOnly.py
@@ -1,23 +1,27 @@
 from functools import wraps
+from traceback import format_exc as err
 
-from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+from pyrogram.errors.exceptions.forbidden_403 import \
+    ChatWriteForbidden
 from pyrogram.types import Message
 from pyrogram import Client as app
 
 from DaisyXMusic.config import SUDO_USERS
 from DaisyXMusic.helpers.perms import member_permissions
 
-async def authorised(func, subFunc2, client, message, *args, **kwargs):
+
+async def authorised(
+    func, subFunc2, client, message, *args, **kwargs
+):
     chatID = message.chat.id
     try:
         await func(client, message, *args, **kwargs)
     except ChatWriteForbidden:
         await app.leave_chat(chatID)
     except Exception as e:
-        try:
-            await message.reply_text(str(e))
-        except ChatWriteForbidden:
-            await app.leave_chat(chatID)
+        await message.reply_text(str(e.MESSAGE))
+        e = err()
+        print(str(e))
     return subFunc2
 
 
@@ -43,14 +47,26 @@ def adminsOnly(permission):
                 # For anonymous admins
                 if message.sender_chat:
                     return await authorised(
-                        func, subFunc2, client, message, *args, **kwargs
+                        func,
+                        subFunc2,
+                        client,
+                        message,
+                        *args,
+                        **kwargs,
                     )
-                return await unauthorised(message, permission, subFunc2)
+                return await unauthorised(
+                    message, permission, subFunc2
+                )
             # For admins and sudo users
             userID = message.from_user.id
             permissions = await member_permissions(chatID, userID)
-            if userID not in SUDO_USERS and permission not in permissions:
-                return await unauthorised(message, permission, subFunc2)
+            if (
+                userID not in SUDOERS
+                and permission not in permissions
+            ):
+                return await unauthorised(
+                    message, permission, subFunc2
+                )
             return await authorised(
                 func, subFunc2, client, message, *args, **kwargs
             )
