622afdc6da9c2783579d997fc98a775f504d752f
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-07-14 03:15:02 +0530

    Update __init__.py

--- DaisyXMusic/__init__.py
+++ DaisyXMusic/__init__.py
@@ -4,8 +4,8 @@ from DaisyXMusic.config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
     "ErenPyro",
-    API_ID,
-    API_HASH,
+   api_id=API_ID,
+    api_hash=API_HASH,
     bot_token=BOT_TOKEN,
 )
 
