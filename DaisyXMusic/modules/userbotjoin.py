059d66f2d0a67a6d2c751204235cec24292ea60c
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-07-31 23:05:30 +0530

    Update userbotjoin.py

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -19,13 +19,13 @@ from pyrogram import Client
 from pyrogram import filters
 from pyrogram.errors import UserAlreadyParticipant
 import asyncio
-from DaisyXMusic.helpers.decorators import authorized_users_only
+from DaisyXMusic.helpers.adminsOnly import adminsOnly
 from DaisyXMusic.helpers.decorators import errors
 from DaisyXMusic.services.callsmusic.callsmusic import client as USER
 from DaisyXMusic.config import SUDO_USERS
 
 @Client.on_message(filters.command(["userbotjoin"]) & ~filters.private & ~filters.bot)
-@authorized_users_only
+@adminsOnly("can_manage_voice_chats")
 @errors
 async def addchannel(client, message):
     chid = message.chat.id
@@ -52,8 +52,7 @@ async def addchannel(client, message):
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @MikasaMusicPlayer to your Group and try again</b>",
+            f"Error Occurred! Make sure Mikasa is not banned in the group!"
         )
         return
     await message.reply_text(
@@ -62,7 +61,7 @@ async def addchannel(client, message):
 
 
 @USER.on_message(filters.group & filters.command(["userbotleave"]))
-@authorized_users_only
+@adminsOnly("can_manage_voice_chats")
 async def rem(USER, message):
     try:
         await USER.leave_chat(message.chat.id)
@@ -91,46 +90,3 @@ async def bye(client, message):
         await client.send_message(message.chat.id, f"Left {left} chats. Failed {failed} chats.")
     
     
-@Client.on_message(filters.command(["userbotjoinchannel","ubjoinc"]) & ~filters.private & ~filters.bot)
-@authorized_users_only
-@errors
-async def addcchannel(client, message):
-    try:
-      conchat = await client.get_chat(message.chat.id)
-      conid = conchat.linked_chat.id
-      chid = conid
-    except:
-      await message.reply("Is chat even linked")
-      return    
-    chat_id = chid
-    try:
-        invitelink = await client.export_chat_invite_link(chid)
-    except:
-        await message.reply_text(
-            "<b>Add me as admin of yor channel first</b>",
-        )
-        return
-
-    try:
-        user = await USER.get_me()
-    except:
-        user.first_name = "Mikasa"
-
-    try:
-        await USER.join_chat(invitelink)
-        await USER.send_message(message.chat.id, "I joined here as you requested")
-    except UserAlreadyParticipant:
-        await message.reply_text(
-            "<b>helper already in your channel</b>",
-        )
-        return
-    except Exception as e:
-        print(e)
-        await message.reply_text(
-            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @MikasaMusicPlayer to your Group and try again</b>",
-        )
-        return
-    await message.reply_text(
-        "<b>helper userbot joined your channel</b>",
-    )
