43aaa6c680756cab79d96af2343cf9a43af0174f
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-08-21 01:59:24 +0530

    i love redis :)
    Adding database to store the chats in /musicplayer on/off

---
+++ DaisyXMusic/database.py
@@ -0,0 +1,26 @@
+from DaisyXMusic import musicdb
+
+try:
+    musicdb.get("MusicChats")
+except BaseException:
+    musicdb.set("MusicChats", "[]")
+
+def is_music(chat_id):
+    list = musicdb.get("MusicChats")	
+    if chat_id in list:
+    	return True
+    return False
+	   
+def set_music(chat_id):
+    list = musicdb.get("MusicChats")
+    if chat_id not in list:
+    	list.append(chat_id)
+    	musicdb.set("MusicChats", str(list))
+    return 
+	
+def rem_music(chat_id):
+	list = musicdb.get("MusicChats")
+	if chat_id in list:
+		list.remove(chat_id)
+		musicdb.set("MusicChats", str(list))
+	return 
