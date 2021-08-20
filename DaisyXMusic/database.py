e16b054d7779c66da3b7ddc3bba906900fa8ed95
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-08-21 03:07:31 +0530

    Now correct

--- DaisyXMusic/database.py
+++ DaisyXMusic/database.py
@@ -1,25 +1,25 @@
 from DaisyXMusic import musicdb
 
 try:
-    musicdb.get("MusicChats")
+    eval(musicdb.get("MusicChats"))
 except BaseException:
     musicdb.set("MusicChats", "[]")
 
 def is_music(chat_id):
-    list = musicdb.get("MusicChats")	
+    list = eval(musicdb.get("MusicChats"))	
     if chat_id in list:
     	return True
     return False
 	   
 def set_music(chat_id):
-    list = musicdb.get("MusicChats")
+    list = eval(musicdb.get("MusicChats"))
     if chat_id not in list:
     	list.append(chat_id)
     	musicdb.set("MusicChats", str(list))
     return 
 	
 def rem_music(chat_id):
-	list = musicdb.get("MusicChats")
+	list = eval(musicdb.get("MusicChats"))
 	if chat_id in list:
 		list.remove(chat_id)
 		musicdb.set("MusicChats", str(list))
